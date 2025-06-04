984d52990f1aa809fa739b65a8ed228a42905f08
SangWon7242 <gotkdqja@gmail.com>
2025-06-04 20:22:41 +0900

    숫자 기록을 별도의 함수로 구현

--- src/app/state/page.tsx
+++ src/app/state/page.tsx
@@ -5,12 +5,18 @@ import { useState } from "react";
 export default function State() {
   const [count, setCount] = useState(0);
 
+  const increaseNum = () => setCount(count + 1);
+  const decreaseNum = () => setCount(count - 1);
+  const resetNum = () => setCount(0);
+
   return (
     <div>
       <div>숫자 : {count}</div>
-      <button onClick={() => setCount(count + 1)}>증가</button>
+      <button onClick={increaseNum}>증가</button>
       &nbsp;
-      <button onClick={() => setCount(count - 1)}>감소</button>
+      <button onClick={decreaseNum}>감소</button>
+      &nbsp;
+      <button onClick={resetNum}>초기화</button>
     </div>
   );
 }
