e482cb6dd2d7a296be9909b24fca20a644f316bc
SangWon7242 <gotkdqja@gmail.com>
2025-06-04 19:33:42 +0900

    스타일을 하는 여러가지 방법과 컴포넌트 개념의 이해

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+const square = {
+  width: "200px",
+  height: "200px",
+  backgroundColor: "green",
+};
+
+const Square2 = () => {
+  const square = {
+    width: "200px",
+    height: "200px",
+    backgroundColor: "purple",
+  };
+
+  return <div style={square}></div>;
+};
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div
+        style={{ width: "200px", height: "200px", backgroundColor: "red" }}
+      ></div>
+
+      <div className="w-[200px] h-[200px] bg-blue-500"></div>
+      <div style={square}></div>
+      <Square2 />
+    </>
+  );
 }
